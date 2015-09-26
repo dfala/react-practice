2f9c843239c3b16fe5cb24054ca5d55b84dae4cd
Daniel Falabella <dnlfala@gmail.com>
2015-09-26 10:12:34 -0600

    first commit

---
+++ src/views/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Banner extends React.Component {
+  render() {
+    return (
+      <div className="banner">
+        This is my banner
+      </div>
+    );
+  }
+}
+
+export default (Banner);
---
+++ src/views/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Header extends React.Component {
+  render () {
+    return (
+      <header className='l-header-separated'>
+        <div className="l-left-box">
+					<div>LOGO</div>
+				</div>
+      </header>
+    );
+  }
+}
+
+export default (Header);
