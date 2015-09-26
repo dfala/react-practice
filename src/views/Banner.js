02b6de173372d7a5023d780154c45deb85930d77
Daniel Falabella <dnlfala@gmail.com>
2015-09-26 11:41:43 -0600

    Added some extra stuff on banner.

--- src/views/Banner.js
+++ src/views/Banner.js
@@ -4,7 +4,13 @@ export class Banner extends React.Component {
   render() {
     return (
       <div className="banner">
-        This is my banner
+        <div className="container">
+          <h1>This is a banner.</h1>
+          <form>
+            <input type="text" placeholder="email" />
+            <input type="text" placeholder="password" />
+          </form>
+        </div>
       </div>
     );
   }
